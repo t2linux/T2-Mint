7fec5bd5f6984ed0c1b4487967ff01bd5f16624a
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2025-02-19 16:14:42 +0530

    6.13.3

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ IMAGE_PATH="${WORKING_PATH}/image"
 CODENAME=noble
 FLAVOUR=$1
 MINT_VERSION=22.1
-KERNEL_VERSION=6.12.9
+KERNEL_VERSION=6.13.3
 PKGREL=1
 sed -i "s/KVER/${KERNEL_VERSION}/g" $(pwd)/files/chroot_build.sh
 sed -i "s/PREL/${PKGREL}/g" $(pwd)/files/chroot_build.sh
